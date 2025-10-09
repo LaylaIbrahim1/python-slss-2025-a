02b113c32cc62b431234e110d2e0f4f73d4dc33f
Layla Ibrahim <queensusu10@gmail.com>
2025-10-09 14:47:45 -0700

    Added to main

---
+++ notes-2-methods.py
@@ -0,0 +1,16 @@
+# Methods
+#  Author: Layla
+# 6 October 2025
+#
+# Ask the user whats the weather like
+weather = input (" What's the weather like right now?")
+
+if weather.lower().strip("!") == "rainy":
+    print (" You should bring an umbrella.")
+else:
+    print (" I see...")
+elif ____________:
+    #'SUNNY' "sunny!" "sunny."
+    print("You should bring a sunscreen.")
+else:
+    print("I see...")
